e8c97a34d774f5d2df369ff0d7e00d7d0bd3b923
takuyahara <h.taku86@gmail.com>
2019-03-18 12:50:53 +0900

    docusaurus

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  /**
+   *
+   *
+   * @returns
+   * @memberof App
+   */
   render() {
     return (
       <div className="App">
